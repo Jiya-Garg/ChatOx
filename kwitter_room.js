132c0abdb383804b2595c0bd2bec72acfd13034a
Jiya-Garg <69499533+Jiya-Garg@users.noreply.github.com>
2021-09-27 16:52:15 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/8.6.5/firebase-database.js"></script>
+var firebaseConfig = {
+    apiKey: "AIzaSyAg4DDDshFA7jKNM62b_wv96l7zwCqzgjo",
+    authDomain: "chatox-15149.firebaseapp.com",
+    projectId: "chatox-15149",
+    storageBucket: "chatox-15149.appspot.com",
+    messagingSenderId: "877536217865",
+    appId: "1:877536217865:web:bee310ebffc30c996761d8",
+    measurementId: "G-PKC596FQED"
+  };
+  
+  // Initialize Firebase
+  firebase = initializeApp(firebaseConfig);
